fb29181c620ed539e81cb8a07a1c7e2747858357
alekspanteli <alekspanteli@rambler.ru>
2022-11-01 13:42:52 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,8 +21,25 @@ const Home = () => {
         latest on web technology so you will never fall behind.
       </p>
 
-      <h3>Featured Teachers</h3>
-      <Link to="teachers/HTML/Tommy-Wingo">Tommy Wingo</Link>
+      <h3 className="mb-5">Featured Teachers</h3>
+      {/* <Link to="teachers/HTML/Tommy-Wingo" className="inline-block mb-5">
+        Tommy Wingo
+      </Link> */}
+
+      <h6 className="text-xl text-sky-600 mb-5">Featured teachers</h6>
+      <form className="flex gap-1 items-center">
+        <input
+          className="block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"
+          placeholder="Name"
+        />
+        <input
+          className="block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm"
+          placeholder="Name"
+        />
+        <button className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm">
+          Go !
+        </button>
+      </form>
     </div>
   );
 };
